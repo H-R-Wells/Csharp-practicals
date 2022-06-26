05fd592d8a85cb1bb3dd77d434a09dd74e218008
H. R. Wells <100534001+H-R-Wells@users.noreply.github.com>
2022-06-26 18:27:57 +0530

    First commit

---
+++ Temp/Temp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            int vowels = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+                    vowels++;
+                }
+            }
+
+            Console.WriteLine("Number of vowels present in the string is " + vowels);
+            Console.ReadKey();
+        }
+    }
+}
