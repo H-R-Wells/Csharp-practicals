07778477a5b7575a24d9bacbb5c3ea7d7cc97d98
H. R. Wells <100534001+H-R-Wells@users.noreply.github.com>
2022-06-29 21:50:23 +0530

    prac 9

---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class temp
+    {
+        String name, hobby;
+
+        public void getData()
+        {
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter hobby");
+            hobby = Console.ReadLine();
+            Console.WriteLine();
+        }
+        public void displayData()
+        {
+            Console.WriteLine(name+"'s hobby is "+hobby);
+        }
+        public String tHobby()
+        {
+            return hobby;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            temp[] info = new temp[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                info[i] = new temp();
+                info[i].getData();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if (info[i].tHobby() == "reading")
+                {
+                    info[i].displayData();
+                }
+            }
+        }
+    }
+}
--- Temp/Temp/Program.cs
+++ Temp/Temp/Program.cs
@@ -6,11 +6,72 @@ using System.Threading.Tasks;
 
 namespace Temp
 {
+
+    public class temp1
+    {
+        String name = "Shubham", hobby;
+
+
+
+        public void getdata()
+        {
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your hobby");
+            hobby = Console.ReadLine();
+        }
+
+
+
+        public void displayData()
+        {
+            Console.WriteLine(name+"'s hobby is " +hobby);
+        }
+
+        public String getHobby()
+        {
+            return hobby;
+        }
+
+
+        /*
+            temp1[] objStaff = new temp1[5];
+            for (int i = 0; i < 2; i++)
+            {
+                objStaff[i] = new temp1();
+                objStaff[i].getdata();
+            }
+            Console.WriteLine(objStaff[0].getHobby());
+            for (int i = 0; i < 2; i++)
+            {
+                if (objStaff[i].getHobby() == "reading")
+                    objStaff[i].displayData();
+            }
+         */
+
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            temp1[] temps = new temp1[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                temps[i] = new temp1();
+                temps[i].getdata();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if (temps[i].getHobby() == "coding")
+                {
+                    temps[i].displayData();
+                }
+
+            }
+
         }
     }
 }
